944c6a9f97b75b62cba1fc8b43dbd39790fd0a07
Marketing Director <nicolestark1103@gmail.com>
2023-07-25 14:35:34 -0500

    carousel working

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,34 @@ const cookieBox = document.querySelector(".cookiewrapper"),
 			let checkCookie = document.cookie.indexOf("CookieBy=CodingNepal"); //checking our cookie
 			//if cookie is set then hide the cookie box else show it
 			checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
+
+// customer testimonial slider
+const carslider = document.querySelector(".carslider");
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector(".right");
+const indicatorParents = document.querySelector(".controls ul");
+
+var sectionIndex = 0;
+
+document.querySelectorAll(".controls li").forEach(function(indicator, ind){
+	indicator.addEventListener("click", function(){
+		sectionIndex = ind;
+		document.querySelector(".controls .selected").classList.remove("selected");
+		indicator.classList.add("selected");
+		carslider.style.transform = "translate(" + (sectionIndex) * -25 + "%)";
+	})
+})
+
+leftArrow.addEventListener("click",function(){
+	sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+	document.querySelector(".controls .selected").classList.remove("selected");
+	indicatorParents.children[sectionIndex].classList.add("selected");
+	carslider.style.transform = "translate(" + (sectionIndex) * -25 + "%)";
+});
+
+rightArrow.addEventListener("click",function(){
+	sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 3;
+	document.querySelector(".controls .selected").classList.remove("selected");
+	indicatorParents.children[sectionIndex].classList.add("selected");
+	carslider.style.transform = "translate(" + (sectionIndex) * -25 + "%)";
+});
